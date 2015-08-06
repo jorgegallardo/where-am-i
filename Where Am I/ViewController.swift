6c180b101ee2b0588e3eaafa6bd0552bc03c05f5
Jorge Gallardo <jorge8486@gmail.com>
2015-08-06 14:25:25 -0400

    update location details in real time

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -30,10 +30,55 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(locations)
-        var userLocation: CLLocation = locations[0] as CLLocation
+        let userLocation: CLLocation = locations[0] as CLLocation
         self.latLabel.text = "\(userLocation.coordinate.latitude)" // to convert to string
         self.longLabel.text = "\(userLocation.coordinate.longitude)"
+        self.courseLabel.text = "\(userLocation.course)"
+        self.speedLabel.text = "\(userLocation.speed)"
+        self.altitudeLabel.text = "\(userLocation.altitude)"
         
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarkData, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                let placemarks = placemarkData as [CLPlacemark]!
+                let loc: CLPlacemark = placemarks[0]
+                
+                var subThoroughfare: String = ""
+                var thoroughfare: String = ""
+                var subLocality: String = ""
+                var locality: String = ""
+                var postalCode: String = ""
+                var country: String = ""
+                var administrativeArea: String = ""
+                
+                if loc.subThoroughfare != nil {
+                    subThoroughfare = loc.subThoroughfare!
+                }
+                if loc.thoroughfare != nil {
+                    thoroughfare = loc.thoroughfare!
+                }
+                if loc.subLocality != nil {
+                    subLocality = loc.subLocality!
+                }
+                if loc.locality != nil {
+                    locality = loc.locality!
+                }
+                if loc.postalCode != nil {
+                    postalCode = loc.postalCode!
+                }
+                if loc.country != nil {
+                    country = loc.country!
+                }
+                if loc.administrativeArea != nil {
+                    administrativeArea = loc.administrativeArea!
+                }
+                
+                self.nearestAddressLabel.text = "\(subThoroughfare) " + "\(thoroughfare)" + "\(subLocality) " + "\n" + "\(locality) " + "\(administrativeArea) " + "\(postalCode)" + "\n" + "\(country)"
+            }
+        }
+    }
+
 //        let userLocation: CLLocation = locations[0] as CLLocation // cast as
 //        let latitude = userLocation.coordinate.latitude
 //        let longitude = userLocation.coordinate.longitude
@@ -43,7 +88,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 //        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
 //        let region: MKCoordinateRegion =  MKCoordinateRegionMake(location, span)
 //        map.setRegion(region, animated: true)
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
